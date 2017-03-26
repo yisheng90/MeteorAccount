69c3cfb101313c032193ada91dc98167c79575a0
yisheng90 <yisheng@yishenglee.com>
2017-03-26 11:03:29 +0800

    theprochallenge

---
+++ client/templates/passwordreset/forgotpassword.js
@@ -0,0 +1,16 @@
+Template.forgotpassword.events({
+  'submit form': function (e, template) {
+    e.preventDefault()
+
+    let email = template.find('[name=email]').value
+
+    Meteor.call('requestPasswordToken', email, (error, token) => {
+      template.find('.error').textContent = ''
+      if(error) {
+        template.find('.error').textContent = "Can't locate user, please try again";
+      } else {
+        console.log(token)
+      }
+    })
+  }
+})
--- server/methods/methods_passwordreset.js
+++ server/methods/methods_passwordreset.js
@@ -1,7 +1,14 @@
 Meteor.methods({
   authResetPassword (passwordInfo) {
     console.log('resetPassword', passwordInfo)
-    var user = Accounts.setPassword( passwordInfo.user_id, passwordInfo.newPassword);
+    let user = Accounts.setPassword( passwordInfo.user_id, passwordInfo.newPassword);
     return user;
+  },
+  requestPasswordToken (email) {
+    let user = Accounts.findUserByEmail(email)
+    let user_id = user._id
+
+    let token = Accounts.sendResetPasswordEmail(user_id, email)
+    return token
   }
 });
