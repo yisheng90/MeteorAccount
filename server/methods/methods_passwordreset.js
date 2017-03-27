e720c8dbcae1a9eb171d658d1f89be156104ac10
yisheng90 <yisheng@yishenglee.com>
2017-03-27 09:42:28 +0800

    to merge

--- server/methods/methods_passwordreset.js
+++ server/methods/methods_passwordreset.js
@@ -1,14 +1,6 @@
 Meteor.methods({
   authResetPassword (passwordInfo) {
-    console.log('resetPassword', passwordInfo)
-    let user = Accounts.setPassword( passwordInfo.user_id, passwordInfo.newPassword);
-    return user;
-  },
-  requestPasswordToken (email) {
-    let user = Accounts.findUserByEmail(email)
-    let user_id = user._id
-
-    let token = Accounts.sendResetPasswordEmail(user_id, email)
-    return token
+    var user = Accounts.setPassword(passwordInfo.user_id, passwordInfo.newPassword)
+    return user
   }
-});
+})
